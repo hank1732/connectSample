6dd47f1b921b18ef1bf2f95f4881ff11ac86c5a9
Hank Han <bhan2@cisco.com>
2016-03-16 16:15:46 +0800

    bugfix: sse caused by res.end

--- SeverSendEvent/app.js
+++ SeverSendEvent/app.js
@@ -6,22 +6,29 @@ var count = 0,
     time = 0;
 
 app.use(function(req, res, next) {
-  res.header('Content-Type', 'text/event-stream');
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+  });
   next();
 });
 
 app.all('/data', function(req, res) {
     console.log('Request comes!');
     count++;
-    time = new Date().getMilliseconds();
-    res.write('id: ' + count + '\n');
-    res.write("data: " + time + '\n\n');
-    if(time%2 === 0){
-        res.write('event: ' + 'ping' + '\n');
-    }
-    res.end();
+    setInterval(function (){
+      time = new Date().getMilliseconds();
+      res.write('id: ' + count + '\n');
+      res.write("data: " + time + '\n\n');
+      if(time%2 === 0){
+          res.write('event: ' + 'ping' + '\n');
+      }
+    },1000);
+    
+    // res.end();
 });
 
 var server = app.listen(port, function() {
@@ -29,3 +36,34 @@ var server = app.listen(port, function() {
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
 });
+
+// var http = require('http');
+
+// http.createServer(function(req, res) {
+//     sendSSE(req, res);
+//     console.log('requer come!')
+// }).listen(8020);
+
+// function sendSSE(req, res) {
+//   res.writeHead(200, {
+//     "Access-Control-Allow-Origin": "*",
+//     "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+//     'Content-Type': 'text/event-stream',
+//     'Cache-Control': 'no-cache',
+//     'Connection': 'keep-alive'
+//   });
+
+//   var id = (new Date()).toLocaleTimeString();
+
+//   // Sends a SSE every 5 seconds on a single connection.
+//   setInterval(function() {
+//     constructSSE(res, id, (new Date()).toLocaleTimeString());
+//   }, 5000);
+
+//   constructSSE(res, id, (new Date()).toLocaleTimeString());
+// }
+
+// function constructSSE(res, id, data) {
+//   res.write('id: ' + id + '\n');
+//   res.write("data: " + data + '\n\n');
+// }
--- SeverSendEvent/client.js
+++ SeverSendEvent/client.js
@@ -1,11 +1,10 @@
-var es = new EventSource("http://" + host + ":8020/data");
+var es = new EventSource("http://" + 'localhost' + ":8020/data");
 var p = document.getElementsByTagName('p')[0];
 
 es.onmessage = function (event) {
   console.log(event.data);
   p.innerHTML += 'server respones message ' + event.data + '<br>';
 };
-
 
 es.addEventListener('ping', function (event) {
   console.log(event.data);
